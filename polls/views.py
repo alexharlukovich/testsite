5e0a57873c85ccca80209ee9512dc2ace85a86fe
Your Name <you@example.com>
2022-01-16 23:04:08 +0100

    bd + models

---
+++ polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hi there! You are at the polls index!")
+
+
+
+# Create your views here.
