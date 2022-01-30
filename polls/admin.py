b191fbe0dc33375028c29609542fd8d700199cc1
Your Name <you@example.com>
2022-01-30 22:08:09 +0100

    7part

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
-admin.site.register(Question)
+from .models import Question, Choise
+
+class ChoiseInLine(admin.TabularInline):
+    model = Choise
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiseInLine]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
+
 
 # Register your models here.
